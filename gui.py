a44eddfd0e4ab19504513fc45f8b383d4118e206
skawy <elskawyahmedfci>
2022-09-21 08:04:41 +0200

    making mixing script and editing ui

--- gui.py
+++ gui.py
@@ -1,16 +1,18 @@
 import PySimpleGUI as sg
 import pygame
+from pydub import AudioSegment
 
 def gui():
     audio_player_column = [
-        [sg.Text("Choose a file: "), sg.Input(), sg.FileBrowse(key="-SOUND_PATH-")],
+        # [sg.Text("Choose a file: "), sg.Input(), sg.FileBrowse(key="-SOUND_PATH-")],
         [sg.Text(size=(12, 1), key='-STATUS-')],
         [
             sg.Button('Play', pad=(10, 0), key="-PLAY-"),
             sg.Button('Pause', pad=(10, 0), key="-PAUSE-"),
             sg.Button('Stop', pad=(10, 0), key="-STOP-"),
-            sg.Slider(range=(0,100), orientation='h', size=(40, 20), enable_events=True, key="-VOLUME-", default_value=100)
+            sg.Button('Save', pad=(20, 0), key="-SAVE-" , button_color = 'green'),
         ],
+        [sg.Slider(range=(0,100), orientation='h', size=(40, 20), enable_events=True, key="-VOLUME-", default_value=100)],
         [sg.Text(size=(50, 3), key='-SONG-')]
     ]
 
@@ -30,10 +32,11 @@ def gui():
             sg.Column(audio_player_column),
             sg.VSeparator(),
             sg.Column(audio_separator_column),
+
         ]
     ]
 
-    audio_player_window=sg.Window('Audio Player', layout, finalize=True)
+    audio_player_window=sg.Window('Music Mixer', layout, finalize=True)
 
     pygame.init()
     pygame.mixer.init()
@@ -43,26 +46,27 @@ def gui():
         event, values = audio_player_window.read()
         if event == sg.WIN_CLOSED or event=="Exit":
             break
+        
+        empty_song = AudioSegment.silent(duration=10000)
 
-        sound_path = values["-SOUND_PATH-"]
-        if not sound_path:
-            sg.Popup("No song specified.")
-            continue
+        empty_song.export("/home/skawy/side_projects/music-mixer/saved/empty.wav", format='wav')
+
+       # sound_path = '/home/skawy/side_projects/music-mixer/audio_output/song1/accompaniment.wav'
+        sound_path = '/home/skawy/side_projects/music-mixer/saved/empty.wav'
+        # sound_path = values["-SOUND_PATH-"]
+        # if not sound_path:
+        #     sg.Popup("No song specified.")
+        #     continue
 
         song = pygame.mixer.Sound(sound_path)
-        song_length = song.get_length()
+        # song_length = song.get_length()
         song_channel = pygame.mixer.Channel(2)
 
         if event == '-PLAY-':
             audio_player_window['-STATUS-'].update('Playing')
             audio_player_window['-SONG-'].update(sound_path)
-
-            if is_paused:
-                song_channel.unpause()
-                is_paused = False
-            else:
-                song_channel.play(song)
-
+            song_channel.unpause() if is_paused else song_channel.play(song)
+            is_paused = False
         elif event == '-PAUSE-':
             song_channel.pause()
             audio_player_window['-STATUS-'].update('Paused')
